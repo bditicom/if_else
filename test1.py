8dd53e37f6c23227f5e38f8e1c9c9b27b08ffd46
“bditicom” <bditi.com@gmail.com>
2021-05-09 02:30:20 -0700

    if_else

---
+++ test1.py
@@ -0,0 +1,18 @@
+#a=int(input())
+#if (a==4):
+#    print("this is down. you win ")
+#if (a!=4):
+#else:
+#    print("oops, your gues is wrong")
+#x=input()
+#y=input()
+#if (x>y):
+#    print(x+("is greater"))
+#else:
+#    print(y+("the number is greater"))
+x=int(input())
+y=int(input())
+if (x>y):
+    print(str(x)+("is greater"))
+else:
+   print(str(y)+("the number is greater"))
